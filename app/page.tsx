d9152bc829d965c7ab5231f8c83ebc1f030342e8
Imtiyaz Sayyid <imtiyazsayyidwork@gmail.com>
2024-10-24 00:07:14 +0530

    Fix Build Issue

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <div className="relative z-20 py-8 text-center">
         <h1 className="text-4xl sm:text-7xl mb-4 font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500">
-          Imtiyaz Sayyid's Portfolio
+          Imtiyaz Sayyid&apos;s Portfolio
         </h1>
         <p className="font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500">
           We Are In Development, Stay Tuned!
